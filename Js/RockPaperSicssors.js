5489f5601171a320259b67091343d6aff82b7b5f
Hasan-C <Hasan-C@users.noreply.github.com>
2021-02-07 23:08:26 -0500

    Test

---
+++ Js/RockPaperSicssors.js
@@ -0,0 +1,73 @@
+var user = 0;
+var comp = 0;
+var user_score = 0;
+var ai_score = 0;
+var user_score_display = document.getElementById("user_score");
+var ai_score_display = document.getElementById("ai_score");
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var sicssors = document.getElementById("sicssors");
+var user_played = "";
+var choices = ['r', 'p', 's'];
+var ai_play;
+var ran;
+var ai_and_user_input;
+var result;
+
+user_input();
+function ai_plays(){
+  ran = Math.floor(Math.random() * 3);
+  ai_play = choices[ran];
+  return(ai_play)
+}
+
+function user_input(){
+  rock.addEventListener('click', function (){
+    user_played = 'r';
+    game(user_played);
+  })
+  paper.addEventListener('click', function (){
+    user_played = 'p';
+    game(user_played);
+  })
+  sicssors.addEventListener('click', function (){
+    user_played = 's';
+    game(user_played);
+  })
+}
+
+function game(user_played){
+  ai_play = ai_plays();
+  ai_and_user_input = ai_play+user_played;
+  console.log(ai_and_user_input);
+  switch(ai_and_user_input){
+    case "rs":
+    case "pr":
+    case "sp":
+      result = "ai";
+      break;
+
+    case "sr":
+    case "rp":
+    case "ps":
+      result = "user";
+      break;
+
+    case "ss":
+    case "rr":
+    case "pp":
+      result = "draw";
+      break;
+    }
+    if (result == "ai"){
+      ai_score += 1;
+      ai_score_display.innerHTML = ai_score;
+      console.log("ai");
+    }
+
+    else if (result == "user"){
+      user_score += 1;
+      user_score_display.innerHTML = user_score;
+      console.log("user");
+    }
+}
